2e7157dc238c40538b9911ee5e31f368243b1c7e
Danniels Castillo <danniels.magana@gmail.com>
2021-10-08 15:35:19 -0600

    Instalación SharedPreferences e Implementación de Preferencias del Usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:preferenciasusuario/src/pages/home_page.dart';
 import 'package:preferenciasusuario/src/pages/settings_page.dart';
+import 'package:preferenciasusuario/src/shared_prefs/preferencias_usuario.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  // Agregar este código cuando se realice una operación antes del runApp
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = new PreferenciasUsuario();
+  await prefs.initPrefs();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  final prefs = new PreferenciasUsuario();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Preferencias',
-      initialRoute: HomePage.routeName,
+      initialRoute: prefs.ultimaPagina,
       routes: {
         HomePage.routeName: (BuildContext context) => HomePage(),
         SettingsPage.routeName: (BuildContext context) => SettingsPage()
--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,16 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:preferenciasusuario/src/shared_prefs/preferencias_usuario.dart';
 import 'package:preferenciasusuario/src/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'settings';
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  late bool _colorSecundario;
+  int? _genero;
+  String _nombre = 'Rodri';
+
+  late TextEditingController _textController;
+
+  final prefs = new PreferenciasUsuario();
+
+  @override
+  void initState() {
+    super.initState();
+    prefs.ultimaPagina = SettingsPage.routeName;
+    _genero = prefs.genero;
+    _colorSecundario = prefs.colorSecundario;
+    _textController = new TextEditingController(text: prefs.nombreUsuario);
+  }
+
+  _setSelectedRadio(int? valor) {
+    prefs.genero = valor as int;
+
+    setState(() {
+      _genero = valor ?? 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Ajustes'),
+          backgroundColor: (prefs.colorSecundario) ? Colors.teal : Colors.blue,
         ),
         drawer: MenuWidget(),
-        body: Center(child: Text('Ajustes Page')));
+        body: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                'Settings',
+                style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Divider(),
+            SwitchListTile(
+              value: prefs.colorSecundario,
+              title: Text('Color secundario'),
+              onChanged: (value) {
+                setState(() {
+                  prefs.colorSecundario = value;
+                });
+              },
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text('Masculino'),
+              groupValue: _genero,
+              onChanged: _setSelectedRadio,
+            ),
+            RadioListTile(
+                value: 2,
+                title: Text('Femenino'),
+                groupValue: _genero,
+                onChanged: _setSelectedRadio),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                    labelText: 'Nombre', helperText: 'Nombre del usuario'),
+                onChanged: (value) {
+                  prefs.nombreUsuario = value;
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
